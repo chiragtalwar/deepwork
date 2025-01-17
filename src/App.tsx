d06f18e4f77ae307b6ff16eb3c78a43b2f67dbe4
ChiragTalwar <chiragtalwar90@gmail.com>
2025-01-17 15:35:59 +0530

    feat: improved video room stability and tab switch, 17thjan335

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ import Rooms from './pages/Rooms'
 import { Room } from './pages/Room'
 import Profile from './pages/Profile'
 import LoadingTest from './pages/LoadingTest';
-// import { TestVideoRoom } from './components/room/TestVideoRoom'
+import { TestVideoRoom } from './components/room/TestVideoRoom'
 
 function App() {
   return (
@@ -50,7 +50,7 @@ function App() {
 
                 {/* Add this to your routes */}
                 <Route path="/loading-test" element={<LoadingTest />} />
-                {/* <Route path="/rooms/test" element={<TestVideoRoom />} /> */}
+                <Route path="/rooms/test" element={<TestVideoRoom />} />
               </Routes>
             </main>
             <Toaster />
